765f80328dbf894ab66dba89d75be0b1fe59b64f
BashIsMyOpps <waifuscollector@proton.me>
2025-02-13 01:23:30 +0100

    Made progress on the slots

--- bash_casino.sh
+++ bash_casino.sh
@@ -9,6 +9,51 @@ progressbar(){
 	echo ""
 }
 
+slots(){
+	while true; do
+	echo ""
+	echo "=========="
+	echo "  Slots   "
+	echo "=========="
+	echo "1) Spin"
+	echo "2) Clear"
+	echo "3) Exit"
+	read choice
+	case $choice in
+		1)
+			a=$(( RANDOM % 9 + 1))
+			b=$(( RANDOM % 9 + 1))
+			c=$(( RANDOM % 9 + 1))
+			
+			echo -n "   $a    "
+			sleep 1		
+			echo -n "   $b    "
+			sleep 1
+			echo -n "   $c    "
+			
+			if [[ ($a -eq 7) && ($b -eq 7) && ($c -eq 7) ]]; then
+				echo -n "Congratulations you won XXX money !!!"
+			else 
+				echo -n "You lost"
+			fi
+			;;
+	
+		2)
+			clear
+			;;
+	
+		3)
+			break
+			;;
+
+		*)
+			echo "Wrong command"
+			sleep 1
+			;;
+	esac
+	done
+}
+
 mainmenu(){
 	echo "==================================="
 	echo "    Welcome to the Bash Casino	 "
@@ -25,26 +70,22 @@ while true; do
 
 	case $choice in
 		1)
-			echo "Game in development"
-			sleep 3
-			clear
+			progressbar
+			sleep 2
+			slots
 			;;
-
 		5)
 			echo "You currently have, $money money"
 			sleep 3
-			clear
 			;;
 		6)
 			echo "Exiting casino..."
 			progressbar
 			break
-			clear
 			;;
 		*)
 			echo "Wrong command"
 			sleep 3
-			clear
 			;;
 	esac
 done
